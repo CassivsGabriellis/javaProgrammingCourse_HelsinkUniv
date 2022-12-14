569054415d20a026d0a06b028203f4d447ae038b
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2022-12-14 20:51:05 -0300

    Solutions to the exercises

--- mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
+++ mooc-java-programming-i/part02-Part02_25.Division/src/main/java/Division.java
@@ -1,5 +1,3 @@
-
-
 public class Division {
 
     public static void main(String[] args) {
@@ -7,9 +5,11 @@ public class Division {
         // Once you have implemented the division method, you can
         // try it out here. Fopr example division(3,5);
         // should print "0.6"
-
-        // division(3, 5);
+        division(3, 5);
+        
     }
-
-    // implement the method here
+    public static void division(int numerator, int denominator) {
+        double result = numerator / (double) denominator;
+        System.out.println(result);
+    }
 }
