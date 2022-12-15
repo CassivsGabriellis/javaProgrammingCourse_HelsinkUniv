9689e10159356110866fb2d501df709882d29727
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2022-12-15 00:27:30 -0300

    Solution to part02-Part02_34.AdvancedAstrology

--- mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -3,18 +3,48 @@ public class AdvancedAstrology {
 
     public static void printStars(int number) {
         // part 1 of the exercise
+        int count = 0;
+        while (count < number) {
+            System.out.print("*");
+            count++;
+        }
+        System.out.println("");
     }
 
     public static void printSpaces(int number) {
         // part 1 of the exercise
+        int count = 0;
+        while (count < number) {
+            System.out.print(" ");
+            count++;
+        }
     }
 
     public static void printTriangle(int size) {
         // part 2 of the exercise
+        int spaces = size - 1;
+        for (int i = 1; i <= size; i++) {
+            printSpaces(spaces);
+            printStars(i);
+            spaces--;
+        }
+
     }
 
     public static void christmasTree(int height) {
         // part 3 of the exercise
+        int spaces = height - 1;
+        for (int i = 1; i <= height; i++) {
+            printSpaces(spaces);
+            printStars(i * 2 - 1);
+            spaces--;
+        }
+
+        for (int j = 2; j > 0; j--) {
+            printSpaces(height - 2);
+            printStars(3);
+        }
+
     }
 
     public static void main(String[] args) {
