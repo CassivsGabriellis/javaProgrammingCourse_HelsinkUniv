833b0479899273f4fec3b14aae7c43749976d27e
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-01-08 20:11:43 -0300

    part03-Part03_30.AgeOfTheOldest

--- mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -6,6 +6,19 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int oldestAge = 0;
 
+        while (true) {
+            String entry = scanner.nextLine();
+            if (entry.isEmpty()) {
+                break;
+            }
+            String[] parts = entry.split(",");
+            int age = Integer.parseInt(parts[1].trim());
+            if (age > oldestAge) {
+                oldestAge = age;
+            }
+        }
+        System.out.println("Age of the oldest: " + oldestAge);
     }
 }
