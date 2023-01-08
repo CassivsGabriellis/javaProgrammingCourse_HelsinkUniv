63b1f9c0e5a87286c36bf317f51cf84e837dfffe
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-01-08 18:07:37 -0300

    Solution to part03-Part03_25.Login

--- mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
+++ mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
@@ -5,6 +5,22 @@ public class Login {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        String positiveAlert = "You have successfully logged in!";
+        String negativeAlert = "Incorrect username or password!";
+        
+        System.out.print("Enter username: ");
+        String user = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+        
+        if (user.equals("alex") && password.equals("sunshine")) {
+            System.out.println(positiveAlert);
+        } else if (user.equals("emma") && password.equals("haskell")) {
+            System.out.println(positiveAlert);
+        } else {
+            System.out.println(negativeAlert);
+        }
 
     }
 }
