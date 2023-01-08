b99b1b1eca6073a3e89f12e2b1ae411dc5f1f631
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-01-08 19:34:46 -0300

    Solution to part03-Part03_28.FirstWords

--- mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -5,7 +5,13 @@ public class FirstWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        while (true) {
+            String entry = scanner.nextLine();
+            if (entry.isEmpty()) {
+                break;
+            }
+            String[] wordsToSplit = entry.split("\\s+");
+            System.out.println(wordsToSplit[0]);
+        }
     }
 }
