bfcb8a8c7be842bc557855b66b37717b473fdec6
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-01-08 19:45:32 -0300

    Solution to part03-Part03_29.LastWords

--- mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -6,6 +6,15 @@ public class LastWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String entry = scanner.nextLine();
+            if (entry.isEmpty()) {
+                break;
+            }
+
+            String[] parts = entry.split("\\s+");
+            System.out.println(parts[parts.length - 1]);
+        }
 
     }
 }
