49bb97dec932804c99bfbe036e516bb8746598d1
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-01-08 20:25:32 -0300

    Solution to part03-Part03_31.NameOfTheOldest

--- mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,7 +5,22 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int oldestAge = 0;
+        String nameOfTheOldest = "";
 
-
+        while (true) {
+            String entry = scanner.nextLine();
+            if (entry.isEmpty()) {
+                break;
+            }
+            String[] parts = entry.split(",");
+            int age = Integer.parseInt(parts[1].trim());
+            if (age > oldestAge) {
+                oldestAge = age;
+                nameOfTheOldest = parts[0].trim();
+            }
+        }
+        System.out.println("Name of the oldest: " + nameOfTheOldest);
     }
 }
