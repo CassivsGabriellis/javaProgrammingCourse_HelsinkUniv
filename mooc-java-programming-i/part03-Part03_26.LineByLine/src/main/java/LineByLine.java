add94f489ac39d729d4fc39df416636dc2302d1a
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-01-08 18:35:24 -0300

    Solution to part03-Part03_26.LineByLine

--- mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -6,6 +6,18 @@ public class LineByLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        while (true) {
+            String words = scanner.nextLine();
+            
+            if (words.isEmpty()) {
+                break;
+            }
+            
+            String[] wordsToSplit = words.split("\\s+");
+            for (String word: wordsToSplit) {
+                System.out.println(word);
+            }
+        }
+        
     }
 }
