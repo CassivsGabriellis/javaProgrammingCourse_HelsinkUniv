a7e64c375dcbfb952e25228d5b7712398c5e6d7f
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-01-08 19:34:10 -0300

    Solution to part03-Part03_27.AVClub

--- mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,7 +5,21 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        
+        while (true) {
+            String entry = scanner.nextLine();
+            
+            if (entry.isEmpty()) {
+                break;
+            }
+            
+            String[] wordsToSplit = entry.split("\\s+");
+            for (String word: wordsToSplit) {
+                if(word.contains("av")) {
+                    System.out.println(word);
+                }
+            }
+        }
+        
     }
 }
