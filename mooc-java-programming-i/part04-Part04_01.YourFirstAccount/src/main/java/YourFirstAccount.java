335623623a363778e8174be0b564cd847ccc908d
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-06-12 13:57:37 -0300

    part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java

---
+++ mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,12 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        
+        Account cassivsAccount = new Account ("Cassivs' account", 100);
+        cassivsAccount.deposit(20);
+        System.out.println("Cassivs' account: " + cassivsAccount);
+    }
+}
